8370fbe261c6f566a317b2d179dbf7c5a10e3a93
Namamiri <yaraliburak@hotmail.com>
2014-04-28 11:24:32 +0200

    Datenbank repariert und Gameobjekt aus Prefab laden (Muss noch für obj umgeschrieben werden)

---
+++ Projekt/Assets/CreateCar.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCar : MonoBehaviour {
+
+	public static GameObject fromPrefab(){
+		GameObject Car;
+		Debug.Log("Car");
+		Car = Instantiate(Resources.Load ("Sphere")) as GameObject;
+		Car.transform.Translate (new Vector3 (3, 3, 3));
+		return Car;
+	}
+}
--- Projekt/Assets/StartSimu.cs
+++ Projekt/Assets/StartSimu.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class StartSimu : MonoBehaviour {
-
+	List<GameObject> Cars = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
+
+		Cars.Add(CreateCar.fromPrefab ());
 		Database datenbank = new Database ();;
 		datenbank.Start ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
