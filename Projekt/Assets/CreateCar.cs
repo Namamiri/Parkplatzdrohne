aeb3e5327a17040bc3bd907a9a3cfa07bc5372a1
Namamiri <yaraliburak@hotmail.com>
2014-05-06 01:28:39 +0200

    Scene with Parkplatz. Parkplatz.fbx und auto.fbx nicht mit hochgeladen bitte Nachholen

--- Projekt/Assets/CreateCar.cs
+++ Projekt/Assets/CreateCar.cs
@@ -9,5 +9,21 @@ public class CreateCar : MonoBehaviour {
 		Car = Instantiate(Resources.Load ("Capsule")) as GameObject;
 		Car.transform.Translate (new Vector3 (3, 3, 3));
 		return Car;
+
+	}
+
+	public static GameObject fromfbx(){
+		GameObject Car;
+		Car = Instantiate(Resources.Load("auto")) as GameObject;
+		string naming = "SO";
+
+		naming = naming + System.Convert.ToChar(Random.Range (65, 90));
+		naming = naming + System.Convert.ToChar(Random.Range (65, 90));
+		naming = naming + Random.Range (25, 999);
+		Car.name = naming;
+		Car.transform.localScale= new Vector3(10,10,10);
+		Car.transform.position = new Vector3 (Random.Range (-5, 5),0,  Random.Range (-5, 5));
+		return Car;
+
 	}
 }
