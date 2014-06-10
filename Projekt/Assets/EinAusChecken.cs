ec0d5e06da781f0584a11637e6fe5ad548daebe2
Namamiri <yaraliburak@hotmail.com>
2014-06-10 23:22:18 +0200

    Neue Class Steuerung Ein und Auschecken

---
+++ Projekt/Assets/EinAusChecken.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EinAusChecken : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.N)) {
+			Debug.Log("Neues Auto wurde Gedrückt");
+		}
+		if (Input.GetKeyDown (KeyCode.Minus)) {
+			Debug.Log ("Auto wurde entfernt");
+			
+		}
+	}
+}
